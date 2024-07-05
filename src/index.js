634d4c037354e3cf21f62053e1aec7a0f69b604f
JacklinPrempeh <JackieP6132@gmail.com>
2024-07-05 15:34:43 +0000

    made changes

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function moreimages(){
+    let m=document.getElementById(more);
+    m.style.display=('block')
+
+}
+
+let extraone=document.getElementById('extra');
+extraone.addEventListener('click',moreimages);
