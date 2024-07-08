17d98251189f4517b8cd7cebe03c4fea35723cfe
JacklinPrempeh <JackieP6132@gmail.com>
2024-07-08 07:55:08 -0700

    adding images and videos with styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-function changecolor(){
-    let m=document.getElementById("more");
-    m.style.display=('block');
+// function changeColor(){
+//     let m=document.getElementById("photo");
+//     m.style.display=('block');
     
 
+// }
+
+// let extraone=document.getElementById("extra");
+// extraone.addEventListener("dblclick",changeColor); 
+
+
+function changeColor(){
+let name = prompt("What is your name");
+let head=document.getElementById("head");
+head.style.overscrollBehavior="hover";
+head.innerHTML=`👋🏾👋🏾Hi ${name} Welcome to African Choice`;
 }
 
-let extraone=document.getElementById('extra');
-extraone.addEventListener("dblclick",changecolor); 
+let linking=document.getElementById("linking");
+linking.addEventListener('mouseenter',changeColor);
